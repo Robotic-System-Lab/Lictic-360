61822def7b5591d4600f72602e9fd215cb203a4a
SalamPS <salamp@salamp.id>
2025-05-06 19:04:10 +0700

    feat(launch): Added fully configured launch

--- src/segnet/launch/denet.launch.py
+++ src/segnet/launch/denet.launch.py
@@ -2,6 +2,9 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.launch_description_sources import XMLLaunchDescriptionSource 
 import launch_ros.actions
 
 def generate_launch_description():
@@ -9,6 +12,12 @@ def generate_launch_description():
         get_package_share_directory('segnet'),
         'config',
         'denet_params.yaml'
+    )
+    
+    camera_launch = os.path.join(
+        get_package_share_directory('ros_deep_learning'),
+        'launch',
+        'video_source.ros2v2.launch'
     )
 
     return LaunchDescription([
@@ -18,5 +27,27 @@ def generate_launch_description():
             name='denet',
             output='screen',
             parameters=[config_file],
+        ),
+        launch_ros.actions.Node(
+            package='visual',
+            executable='qt',
+            name='qt',
+            output='screen',
+        ),
+        launch_ros.actions.Node(
+            package='gmapper',
+            executable='semap',
+            name='semap',
+            output='screen',
+        ),
+        launch_ros.actions.Node(
+            package='merger',
+            executable='odom',
+            name='odom',
+            output='screen',
+        ),
+        IncludeLaunchDescription(
+            XMLLaunchDescriptionSource(camera_launch),
+            launch_arguments={'gui': 'false'}.items(),
         ),
     ])
