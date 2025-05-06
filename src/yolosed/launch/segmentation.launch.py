2b914b0c7138ad8c81865208fad6dd5be0eeb5df
SalamPS <salamp@salamp.id>
2025-05-06 12:28:47 +0700

    feat(launch): Added launch file for yolosed segmentation

--- src/yolosed/launch/segmentation.launch.py
+++ src/yolosed/launch/segmentation.launch.py
@@ -2,6 +2,8 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 import launch_ros.actions
 
 def generate_launch_description():
@@ -9,6 +11,12 @@ def generate_launch_description():
         get_package_share_directory('yolosed'),
         'config',
         'segmentation_params.yaml'
+    )
+    
+    turtlebot3_launch = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'launch',
+        'callibrate_cam.launch.py'
     )
 
     return LaunchDescription([
@@ -18,5 +26,21 @@ def generate_launch_description():
             name='segmentation',
             output='screen',
             parameters=[config_file],
+        ),
+        launch_ros.actions.Node(
+            package='visual',
+            executable='qt',
+            name='qt',
+            output='screen',
+        ),
+        launch_ros.actions.Node(
+            package='gmapper',
+            executable='semap',
+            name='semap',
+            output='screen',
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(turtlebot3_launch),
+            launch_arguments={'gui': 'false'}.items(),
         ),
     ])
