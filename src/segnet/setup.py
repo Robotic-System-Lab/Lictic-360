e3adf980179dcba3bb55ab474bc8bfe063b52970
SalamPS <salamp@salamp.id>
2025-05-06 11:57:23 +0700

    chore(launch): Renamed

--- src/segnet/setup.py
+++ src/segnet/setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/segnet.launch.py']),
+        ('share/' + package_name + '/launch', ['launch/denet.launch.py']),
         ('share/' + package_name + '/config', ['config/denet_params.yaml']),
     ],
     install_requires=['setuptools'],
