72b3fb6646731b137acc2667e9daccc9503396a7
SalamPS <salamp@salamp.id>
2025-07-07 22:02:43 +0700

    feat(map): Generated 3 map types simultaneously

--- src/utils/visual/visual/qtnode.py
+++ src/utils/visual/visual/qtnode.py
@@ -115,65 +115,50 @@ class MainWindow(QMainWindow):
   def redraw_grid(self):
     print(f"Redrawing grid (800x800)")
     pixmap = QPixmap(800, 800)
+    
     pixmap.fill(Qt.transparent)
-    painter = QPainter(pixmap)
+    pixmaps = [pixmap.copy() for _ in range(3)]
+    painters = [QPainter(p) for p in pixmaps]
+    
     for row in range(self.grid_height):
-        real_row = self.grid_height - 1 - row
-        for col in range(self.grid_width):
-            index = real_row * self.grid_width + col
-            value = self.grid_data[index]
-            if value == 99:
-                color = QColor(50, 50, 50)
-                # color = QColor('lightgray')
-            elif value == -1:
-                color = QColor(0, 0, 0, 0)
-            elif value == 0:
-                color = QColor('lightgray')
-            else:
-                ratio = (value - 1) / 9
-                r = int(ratio * 255)
-                b = 255 - r
-                color = QColor(r, 100, b)
+      real_row = self.grid_height - 1 - row
+      for col in range(self.grid_width):
+        index = real_row * self.grid_width + col
+        value = self.grid_data[index]
+        if value == 99:
+          color = [QColor(50, 50, 50) for _ in range(3)]
+        elif value == -1:
+          color = [QColor('darkgray'), None, None]
+        elif value == 0:
+          color = [QColor('lightgray'), None, None]
+        else:
+          ratio = (value - 1) / 9
+          r = int(ratio * 255)
+          b = 255 - r
+          color = [QColor(r, 100, b), QColor(r, 100, b), QColor(50, 50, 50)]
+        for i, fill in enumerate(color):
+          if fill is not None:
             rect = QRectF(col * self.cell_size, row * self.cell_size,
                           self.cell_size, self.cell_size)
-            painter.fillRect(rect, QBrush(color))
-    painter.end()
+            painters[i].fillRect(rect, QBrush(fill))
+    for painter in painters:
+      painter.end()
     
-    folder = os.path.join(os.path.expanduser("~"), "lictic", "captured_map", str(self.start_time), "painted")
-    os.makedirs(folder, exist_ok=True)
-    full_path = f"{folder}/{self.save_indexer}.png"
-    pixmap.save(full_path, "PNG")
+    timestamp_now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    main_folder = os.path.join(os.path.expanduser("~"), "lictic", "captured_map", str(self.start_time))
+    paths = ['full', 'painted_edge', 'plain_edge']
+    for i, path in enumerate(paths):
+      folder = os.path.join(main_folder, path)
+      os.makedirs(folder, exist_ok=True)
+      full_path = f"{folder}/{timestamp_now}.png"
+      pixmaps[i].save(full_path, "PNG")
     
-    if self.save_indexer % 3 == 0:
-      loaded_pixmap = QPixmap(full_path)
-      self.scene.clear()
-      self.scene.addPixmap(loaded_pixmap)
-  
-  def redraw_plain_grid(self):
-    print(f"Redrawing plain grid (800x800)")
-    pixmap = QPixmap(800, 800)
-    pixmap.fill(Qt.transparent)
-    painter = QPainter(pixmap)
-    for row in range(self.grid_height):
-        real_row = self.grid_height - 1 - row
-        for col in range(self.grid_width):
-            index = real_row * self.grid_width + col
-            value = self.grid_data[index]
-            if value == -1:
-                color = QColor(0, 0, 0, 0)
-            elif value == 0:
-                color = QColor('lightgray')
-            else:
-                color = QColor(50, 50, 50)
-            rect = QRectF(col * self.cell_size, row * self.cell_size,
-                          self.cell_size, self.cell_size)
-            painter.fillRect(rect, QBrush(color))
-    painter.end()
-    
-    folder = os.path.join(os.path.expanduser("~"), "lictic", "captured_map", str(self.start_time), "plain")
-    os.makedirs(folder, exist_ok=True)
-    full_path = f"{folder}/{self.save_indexer}.png"
-    pixmap.save(full_path, "PNG")
+    # self.save_indexer += 1
+    # if self.save_indexer % 3 == 0:
+    #   self.save_indexer = 0
+    #   loaded_pixmap = QPixmap(full_path)
+    #   self.scene.clear()
+    #   self.scene.addPixmap(loaded_pixmap)
   
   def update_grid_from_map(self, msg):
     self.grid_width = msg.info.width
@@ -181,8 +166,6 @@ class MainWindow(QMainWindow):
     if self.grid_width > 0:
       self.cell_size = 800 / self.grid_width
     self.grid_data = list(msg.data)
-    self.save_indexer += 1
-    self.redraw_plain_grid()
     self.redraw_grid()
   
   def create_legend_item(self, value):
